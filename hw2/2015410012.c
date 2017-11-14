0bb8b2b250e4ab20432476878326bcb70ea0f509
Noverish <embrapers263@gmail.com>
2017-11-15 01:33:25 +0900

    finished

--- hw2/2015410012.c
+++ hw2/2015410012.c
@@ -8,12 +8,12 @@
 #define SERVER_PORT 47500
 #define MAX_LINE 256
 
-#define FLAG_HELLO		((uint8_t)(0x01 << 7))
+#define FLAG_HELLO		    ((uint8_t)(0x01 << 7))
 #define FLAG_INSTRUCTION	((uint8_t)(0x01 << 6))
 #define FLAG_RESPONSE		((uint8_t)(0x01 << 5))
 #define FLAG_TERMINATE		((uint8_t)(0x01 << 4))
 
-#define OP_ECHO			((uint8_t)(0x00))
+#define OP_ECHO		       	((uint8_t)(0x00))
 #define OP_INCREMENT		((uint8_t)(0x01))
 #define OP_DECREMENT		((uint8_t)(0x02))
 
@@ -32,7 +32,6 @@ int main(int argc, char * argv[]) {
     struct hostent *hp;
     struct sockaddr_in sin;
     char *host = "localhost";
-    char buf[] = "2015410012";
     int s;
 
     /* translate host name into peer's IP address */
@@ -60,12 +59,9 @@ int main(int argc, char * argv[]) {
         exit(1);
     }
 
-    //int len = strlen(buf) + 1;
-    //send(s, buf, len, 0);
-
+    uint32_t student_id = 2015410012;
     uint8_t tmp[4];
-    uint32_t tmp2 = 2015410012;
-    memcpy(tmp, &tmp2, sizeof(uint32_t));
+    memcpy(tmp, &student_id, sizeof(uint32_t));
     send_packet(s, FLAG_HELLO, OP_ECHO, 4, 0, tmp);
 
     while(1) {
@@ -83,13 +79,10 @@ int main(int argc, char * argv[]) {
         printf("\n\n");
 
         if(rcvd_packet.flag == FLAG_INSTRUCTION) {
-
             if(rcvd_packet.operation != OP_ECHO) {
                 uint32_t tmp;
                 memcpy(&tmp, rcvd_packet.data, sizeof(uint32_t));
-
                 tmp += (rcvd_packet.operation == OP_INCREMENT) ? 1 : -1;
-
                 memcpy(rcvd_packet.data, &tmp, sizeof(uint32_t));
             }
 
@@ -104,23 +97,22 @@ int main(int argc, char * argv[]) {
 }
 
 void send_packet(int s, uint8_t flag, uint8_t op, uint16_t len, uint32_t seq, uint8_t *data) {
-    struct hw_packet buf_struct;
-    buf_struct.flag = flag;
-    buf_struct.operation = op;
-    buf_struct.data_len = len;
-    buf_struct.seq_num = seq;
-    memcpy(buf_struct.data, data, len);
+    struct hw_packet send_packet;
+    send_packet.flag = flag;
+    send_packet.operation = op;
+    send_packet.data_len = len;
+    send_packet.seq_num = seq;
+    memcpy(send_packet.data, data, len);
 
-    printf("send flag : %02X\n", buf_struct.flag);
-    printf("send op   : %02X\n", buf_struct.operation);
-    printf("send len  : %04X\n", buf_struct.data_len);
-    printf("send seq  : %08X\n", buf_struct.seq_num);
+    printf("send flag : %02X\n", send_packet.flag);
+    printf("send op   : %02X\n", send_packet.operation);
+    printf("send len  : %04X\n", send_packet.data_len);
+    printf("send seq  : %08X\n", send_packet.seq_num);
     printf("send data : ");
     int i;
-    for(i = 0; i < buf_struct.data_len; i++)
-        printf("%02X", buf_struct.data[i]);
+    for(i = 0; i < send_packet.data_len; i++)
+        printf("%02X", send_packet.data[i]);
     printf("\n\n");
 
-
-    send(s, (char*) &buf_struct, sizeof(struct hw_packet), 0);
+    send(s, (char*) &send_packet, sizeof(struct hw_packet), 0);
 }
