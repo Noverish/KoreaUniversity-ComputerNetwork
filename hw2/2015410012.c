297c0cd4926cd1b52040f547cfd21deb253cc615
Noverish <embrapers263@gmail.com>
2017-11-14 14:46:03 +0900

    Added hw2

---
+++ hw2/2015410012.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdlib.h>
+#define SERVER_PORT 47500
+#define MAX_LINE 256
+
+int main(int argc, char * argv[]) {
+    FILE *fp;
+    struct hostent *hp;
+    struct sockaddr_in sin;
+    char *host = "localhost";
+    char buf[] = "2015410012";
+    int s;
+
+    /* translate host name into peer's IP address */
+    hp = gethostbyname(host);
+    if (!hp) {
+        fprintf(stderr, "simplex-talk: unknown host: %s\n", host);
+        exit(1);
+    }
+
+    /* build address data structure */
+    bzero((char *)&sin, sizeof(sin));
+    sin.sin_family = AF_INET;
+    bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
+    sin.sin_port = htons(SERVER_PORT);
+
+    /* active open */
+    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("simplex-talk: socket");
+        exit(1);
+    }
+
+    if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0) {
+        perror("simplex-talk: connect");
+        close(s);
+        exit(1);
+    }
+
+    int len = strlen(buf) + 1;
+    send(s, buf, len, 0);
+
+    return 0;
+}
+
