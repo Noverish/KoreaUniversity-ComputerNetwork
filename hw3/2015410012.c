8ad5030b5b1b0413d0f416e41fab18c36da7bbe6
Noverish <embrapers263@gmail.com>
2017-11-29 22:07:46 +0900

    Finished hw3

--- hw3/2015410012.c
+++ hw3/2015410012.c
@@ -5,10 +5,12 @@
 #include <netdb.h>
 #include <string.h>
 #include <stdlib.h>
+#include "sha1.h"
 #define SERVER_PORT 47500
 #define MAX_LINE 256
+#define MAX_FILE_SIZE 12288
 
-#define FLAG_HELLO		    ((uint8_t)(0x01 << 7))
+#define FLAG_HELLO		((uint8_t)(0x01 << 7))
 #define FLAG_INSTRUCTION	((uint8_t)(0x01 << 6))
 #define FLAG_RESPONSE		((uint8_t)(0x01 << 5))
 #define FLAG_TERMINATE		((uint8_t)(0x01 << 4))
@@ -16,6 +18,8 @@
 #define OP_ECHO		       	((uint8_t)(0x00))
 #define OP_INCREMENT		((uint8_t)(0x01))
 #define OP_DECREMENT		((uint8_t)(0x02))
+#define OP_PUSH			((uint8_t)(0x03))
+#define OP_DIGEST		((uint8_t)(0x04))
 
 struct hw_packet {
   uint8_t  flag;
@@ -32,7 +36,8 @@ int main(int argc, char * argv[]) {
     struct hostent *hp;
     struct sockaddr_in sin;
     char *host = "localhost";
-    int s;
+    int s, file_index = 0;
+    char file[MAX_FILE_SIZE];
 
     /* translate host name into peer's IP address */
     hp = gethostbyname(host);
@@ -79,14 +84,30 @@ int main(int argc, char * argv[]) {
         printf("\n\n");
 
         if(rcvd_packet.flag == FLAG_INSTRUCTION) {
-            if(rcvd_packet.operation != OP_ECHO) {
+            uint8_t op = rcvd_packet.operation;
+            uint16_t len = rcvd_packet.data_len;
+            uint32_t seq = rcvd_packet.seq_num;
+
+            if(op == OP_INCREMENT || op == OP_DECREMENT) {
                 uint32_t tmp;
                 memcpy(&tmp, rcvd_packet.data, sizeof(uint32_t));
-                tmp += (rcvd_packet.operation == OP_INCREMENT) ? 1 : -1;
+                tmp += (op == OP_INCREMENT) ? 1 : -1;
                 memcpy(rcvd_packet.data, &tmp, sizeof(uint32_t));
-            }
+            
+                send_packet(s, FLAG_RESPONSE, OP_ECHO, len, seq, rcvd_packet.data);
+            } else if(op == OP_PUSH) {
+                printf("    len : %d\n", len);
+                memcpy(&(file[file_index]), rcvd_packet.data, len);
+                printf("    before file_index : %d\n", file_index);
+                file_index += len;
+                printf("    after file_index : %d\n\n", file_index);
 
-            send_packet(s, FLAG_RESPONSE, OP_ECHO, rcvd_packet.data_len, rcvd_packet.seq_num, rcvd_packet.data);
+                send_packet(s, FLAG_RESPONSE, OP_PUSH, 0, 0, NULL);
+            } else if(op == OP_DIGEST) {
+                char *hash_out = malloc(20);
+                SHA1(hash_out, file, file_index);
+                send_packet(s, FLAG_RESPONSE, OP_DIGEST, 20, 0, hash_out);
+            }
         }
 
         if(rcvd_packet.flag == FLAG_TERMINATE)
@@ -102,7 +123,8 @@ void send_packet(int s, uint8_t flag, uint8_t op, uint16_t len, uint32_t seq, ui
     send_packet.operation = op;
     send_packet.data_len = len;
     send_packet.seq_num = seq;
-    memcpy(send_packet.data, data, len);
+    if(data != NULL)
+        memcpy(send_packet.data, data, len);
 
     printf("send flag : %02X\n", send_packet.flag);
     printf("send op   : %02X\n", send_packet.operation);
